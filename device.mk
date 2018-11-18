c4218f5c8220e530e674097948633b698ea10f46
jmjxperia <jmjxperia@users.noreply.github.com>
2018-11-18 08:15:56 +0530

    YUREKA2:implement Doze

--- device.mk
+++ device.mk
@@ -409,6 +409,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    YUDoze
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
