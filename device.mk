0d7bc3a40eeaa423d23dca3f7a60c7b47ed909fd
jmjxperia <jmjxperia@users.noreply.github.com>
2018-11-18 08:27:16 +0530

    YUREKA2: use less camera props

--- device.mk
+++ device.mk
@@ -126,17 +126,13 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    mm-qcamera-app \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
-    libmmcamera_interface \
-    libmmjpeg_interface \
-    libqomx_core \
-    Snap
+	Snap
 
 # Camera shim
 PRODUCT_PACKAGES += \
