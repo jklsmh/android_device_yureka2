d6082e48a5d8a1942f953d73d5408f569df9e2bf
jmjxperia <41988303+jmjxperia@users.noreply.github.com>
2018-12-05 15:08:09 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -246,9 +246,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8953
+     android.hardware.light@2.0-service.aw2013
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
