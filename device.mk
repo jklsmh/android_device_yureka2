7f60b5d48641a203afe61c782b5c56abd05d17ea
jmjxperia <jmjxperia@users.noreply.github.com>
2018-12-08 12:28:08 +0530

    YUREKA2:implement doze

--- device.mk
+++ device.mk
@@ -421,3 +421,7 @@ PRODUCT_COPY_FILES += \
 	PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
+# Doze mode
+PRODUCT_PACKAGES += \
+    YUDoze
